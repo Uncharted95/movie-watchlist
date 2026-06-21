fc526630f468b431b4925189bb4a38bb355c6ca5
Alexander A <41549546+Uncharted95@users.noreply.github.com>
2026-06-21 22:33:24 +0200

    Added rendering code

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const movieTitle = document.querySelector('input[type="search"]').value;
+const btn = document.getElementById("button")
+let films = document.getElementById("films")
 
 async function movieTitles(){
+    const movieTitle = document.querySelector('input[type="search"]').value
     const res = await fetch(`https://www.omdbapi.com/?t=${movieTitle}&apikey=864dce8a`)
     const data = await res.json()
-    console.log(data)
+
+    films.innerHTML = `
+    <img src=${data.Poster} class="film">
+    `
+
 }
+
+
+
+btn.addEventListener("click", movieTitles)
 
