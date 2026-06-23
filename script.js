fef84c52efdcda59e7ad454718f552d2e0a3d81d
Alexander A <41549546+Uncharted95@users.noreply.github.com>
2026-06-23 12:41:27 +0200

    Updated files
    Added lines to the JS code to allow for proper API calls

--- script.js
+++ script.js
@@ -3,16 +3,36 @@ let films = document.getElementById("films")
 
 async function movieTitles(){
     const movieTitle = document.querySelector('input[type="search"]').value
-    const res = await fetch(`https://www.omdbapi.com/?t=${movieTitle}&apikey=864dce8a`)
+    const res = await fetch(`https://www.omdbapi.com/?s=${movieTitle}&apikey=864dce8a`)
     const data = await res.json()
+    
+const movies = await Promise.all(
+    data.Search.map(movie => 
+        fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=864dce8a`)
+        .then(res => res.json())
+    )
+)
 
-    films.innerHTML = `
-    <img src=${data.Poster} class="film">
+let html = ""
+
+movies.forEach(movie => {
+    html += `
+        <div class="film">
+            <img src="${movie.Poster}" class="poster">
+            <div class="text-container">
+                <h3>${movie.Title}</h3>
+                <p>${movie.imdbRating}</p>
+                <p>${movie.Runtime}</p>
+                <p>${movie.Genre}</p>
+                <p>${movie.Plot}</p>
+            </div>
+        </div>
     `
+})
+
+films.innerHTML = html
 
 }
-
-
 
 btn.addEventListener("click", movieTitles)
 
